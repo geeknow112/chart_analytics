2cd73e1bd882b1a78ca276253646f5d46dd76e26
myu <yourenemy1008@gmail.com>
2019-07-22 21:03:54 +0900

    get kabuka

---
+++ sample_selenium.py
@@ -0,0 +1,14 @@
+import time
+from selenium import webdriver
+#import chromedriver_binary
+#https://qiita.com/memakura/items/20a02161fa7e18d8a693
+
+#driver = webdriver.Chrome()
+driver = webdriver.Chrome(executable_path='.\\chromedriver_win32\\chromedriver.exe')
+driver.get('https://www.google.com/')
+time.sleep(5)
+search_box = driver.find_element_by_name("q")
+search_box.send_keys('ChromeDriver')
+search_box.submit()
+time.sleep(5)
+driver.quit()
