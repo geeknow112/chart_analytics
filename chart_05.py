90132ea99e02fd6ff58058965ac1f6ce947664b0
myu <yourenemy1008@gmail.com>
2020-08-19 00:03:21 +0900

    delete csv

--- chart_05.py
+++ chart_05.py
@@ -50,11 +50,76 @@ def main():
     # plt.xticks(range(xtick0,len(df),5), [dt.datetime.strptime(x, '%Y-%m-%d') for x in df.index][xtick0::5])
     plt.xticks(range(xtick0, len(df), 5), [x for x in df.index][xtick0::5])
 
-    term_5, term_20, term_60 = 5, 20, 60
+    term_5, term_7, term_10, term_20, term_60 = 5, 7, 10, 20, 60
     df['av_5'] = df['close'].rolling(window=term_5).mean()
+    df['av_7'] = df['close'].rolling(window=term_7).mean()
+    df['av_10'] = df['close'].rolling(window=term_10).mean()
     df['av_20'] = df['close'].rolling(window=term_20).mean()
     df['av_60'] = df['close'].rolling(window=term_60).mean()
 main()
+
+
+#cnt9 初期化
+cnt = 1
+for i in df.index:
+    df.loc[i, 'cnt9'] = np.nan
+#print(df['cnt9'])
+
+for i in df.index:
+    op = df['open']
+    cl = df['close']
+    av5 = df['av_5']
+    av7 = df['av_7']
+    av10 = df['av_10']
+    av20 = df['av_20']
+    av60 = df['av_60']
+
+    #print(i)
+    #print(op[i])
+    now = df.index.get_loc(i) #行番号取得
+    pre = now - 1
+    av5_p = df.iloc[pre]['av_5']
+    av20_p = df.iloc[pre]['av_20']
+    av60_p = df.iloc[pre]['av_60']
+
+    test = list()
+    test.append(i)
+    test.append('〇') if av5[i] > av20[i] > av60[i] and av5_p < av5[i] and av20_p < av20[i] and av60_p < av60[i] else 'false'
+    print(test)
+
+    # シグナル[下半身、逆下半身]の表示
+    center = op[i] + ((cl[i] - op[i]) * 0.5) # ローソク足の中心値
+    graph_position_up = df['low'][i] * 0.98 #グラフで見やすいようにポジションをずらす
+    graph_position_down = df['hight'][i] * 1.02 #グラフで見やすいようにポジションをずらす
+    k_hn = graph_position_up if cl[i] > av5[i] and center > av5[i] and cl[i] > op[i] else np.nan
+    df.loc[i, 'k_hanshin'] = k_hn if av5[i] > av20[i] > av60[i] and av5_p < av5[i] and av20_p < av20[i] and av60_p < av60[i] else np.nan
+    df.loc[i, 'k_hanshin_2'] = k_hn if av20[i] > av5[i] > av60[i] and av5_p < av5[i] and av20_p < av20[i] and av60_p < av60[i] else np.nan
+    df.loc[i, 'k_hanshin_5'] = k_hn if av60[i] > av5[i] > av20[i] and av5_p < av5[i] and av20_p < av20[i] and av60_p < av60[i] else np.nan
+    df.loc[i, 'k_hanshin_6'] = k_hn if av5[i] > av60[i] > av20[i] and av5_p < av5[i] and av20_p < av20[i] and av60_p < av60[i] else np.nan
+    gk_hn = graph_position_down if cl[i] < av5[i] and center < av5[i] and cl[i] < op[i] else np.nan
+    df.loc[i, 'gk_hanshin'] = gk_hn if av5[i] < av20[i] < av60[i] and av5_p > av5[i] and av20_p > av20[i] and av60_p > av60[i] else np.nan
+    df.loc[i, 'gk_hanshin_2'] = gk_hn if av20[i] < av5[i] < av60[i] and av5_p > av5[i] and av20_p > av20[i] and av60_p > av60[i] else np.nan
+    df.loc[i, 'gk_hanshin_5'] = gk_hn if av60[i] < av5[i] < av20[i] and av5_p > av5[i] and av20_p > av20[i] and av60_p > av60[i] else np.nan
+    df.loc[i, 'gk_hanshin_6'] = gk_hn if av5[i] < av60[i] < av20[i] and av5_p > av5[i] and av20_p > av20[i] and av60_p > av60[i] else np.nan
+
+    # 指標[9の法則]の表示
+    cl_pre = df.iloc[pre]['close']
+    cl_pre2 = df.iloc[pre - 1]['close'] # 2営業前の終値
+    if cl_pre2 < cl_pre < cl[i] or av5[i] < cl[i]:
+        df.loc[i, 'cnt9'] = str(cnt)
+        cnt += 1
+
+    else:
+        cnt = 1
+        df.loc[i, 'cnt9'] = np.nan
+
+    # 指標[草黒赤]の表示
+    print(av5[i], av7[i], av10[i])
+    df.loc[i, 'kka'] = av5[i] * 0.97 if av5[i] > av7[i] > av10[i] else np.nan
+    df.loc[i, 'akk'] = av5[i] * 1.03 if av5[i] < av7[i] < av10[i] else np.nan
+
+
+#exit()
 
 # ゴールデンクロス/デッドクロスしたタイミングの抽出
 def pointCross(status = '5_20', str = '', current_flag = 0, previous_flag = 1):
@@ -89,6 +154,8 @@ pointCross('20_60', 'ded')
 
 def plotMA():
     ax.plot(df.index, df['close'].rolling(5).mean(), color='r', label="MA(5)")
+    ax.plot(df.index, df['close'].rolling(7).mean(), color='black', label="MA(7)")
+    ax.plot(df.index, df['close'].rolling(10).mean(), color='olive', label="MA(10)")
     ax.plot(df.index, df['close'].rolling(20).mean(), color='g', label="MA(20)")
     ax.plot(df.index, df['close'].rolling(60).mean(), color='b', label="MA(60)")
     ax.plot(df.index, df['close'].rolling(75).mean(), color='y', label="MA(75)")
@@ -104,6 +171,26 @@ def scatterPoint():
     plt.scatter(x= df.index,y = df['ded_5_20'],marker='o',color='black')
     plt.scatter(x= df.index,y = df['golden_20_60'],marker='o',color='orange')
     plt.scatter(x= df.index,y = df['ded_20_60'],marker='o',color='pink')
+
+    plt.scatter(x= df.index,y = df['k_hanshin'],marker='^',color='dodgerblue')
+    plt.scatter(x= df.index,y = df['k_hanshin_2'],marker='^',color='cyan')
+    plt.scatter(x= df.index,y = df['k_hanshin_5'],marker='^',color='chartreuse')
+    plt.scatter(x= df.index,y = df['k_hanshin_6'],marker='^',color='darkviolet')
+    plt.scatter(x= df.index,y = df['gk_hanshin'],marker='v',color='dodgerblue')
+    plt.scatter(x= df.index,y = df['gk_hanshin_2'],marker='v',color='cyan')
+    plt.scatter(x= df.index,y = df['gk_hanshin_5'],marker='v',color='chartreuse')
+    plt.scatter(x= df.index,y = df['gk_hanshin_6'],marker='v',color='darkviolet')
+
+    plt.scatter(x= df.index,y = df['kka'],marker='_',color='olive')
+    plt.scatter(x= df.index,y = df['akk'],marker='_',color='olive')
+
+    cnt9 = df['cnt9'].values
+    print(cnt9)
+    for i, d in df.iterrows():
+        #print(d.cnt9)
+        marker = '$' + str(d.cnt9) + '$' if d.cnt9 is not np.nan else ""
+        plt.scatter(x= i,y = d.hight + 25,marker=marker,color='black')
+#        plt.scatter(x= df.index,y = df['hight'] + 75,marker='$' + str(9) + '$',color='black')
 scatterPoint()
 
 ax.grid()
@@ -126,17 +213,17 @@ def zoneColor(str = '', start_dt = '', end_dt = '', dt_20_60 = ''):
         if (np.isnan(start_flag) == False):
             start_dt = dt
             # pprint(dt)
-            pprint(start_dt)
+            #pprint(start_dt)
 
         if (np.isnan(end_flag) == False):
             end_dt = dt
             # pprint(dt)
-            pprint(end_dt)
+            #pprint(end_dt)
 
         if (np.isnan(status_20_60) == False):
             dt_20_60 = dt
             # pprint(dt)
-            pprint(dt_20_60)
+            #pprint(dt_20_60)
 
         if (start_dt and end_dt):
             ax.axvspan(start_dt, end_dt, facecolor=color, alpha=0.1)
