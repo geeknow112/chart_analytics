1c67132f037920643f0b5748261fd57a6d42ded3
myu <yourenemy1008@gmail.com>
2020-08-22 23:22:58 +0900

    move function

--- chart_05.py
+++ chart_05.py
@@ -9,38 +9,30 @@ import numpy as np
 import re
 import pylab
 
-#csv = "../stock_data/9101_2019.csv"
-#csv = "D://Users/z112/source/repos/ConsoleApp2/stock_data/9104_2019.csv"
-csv = "../../../source/repos/chart_gallery/stock_data/9101.csv"
-
-def DataRead():
-    with open(csv, "r") as csv_file:
-        df = pd.read_csv(csv_file, quotechar='"', header=2, index_col=0)
-
-with open(csv, 'r') as csv_file:
-    df = pd.read_csv(csv_file, quotechar='"', header=1, index_col=0, skiprows=range(2, 7500))
-
-df_ = df.copy()
-
-new = [dt.datetime.strptime(i, '%Y-%m-%d') for i in df_.index]
-df_.index = [mdates.date2num(i) for i in new]
-#pprint(df_.index)%exit()
-
-#df_.index = mdates.date2num(df_.index)
-data = df_.reset_index().values
-
 #https://qiita.com/toyolab/items/1b5d11b5d376bd542022
 #https://qiita.com/kjybinp1105/items/db4efd07e20000c22f4e
 
+codes = [9101]
+csv = "../../../source/repos/chart_gallery/stock_data/" + str(codes[0]) + ".csv"
+
+def DataRead():
+    with open(csv, 'r') as csv_file:
+        df = pd.read_csv(csv_file, quotechar='"', header=1, index_col=0, skiprows=range(2, 7200))
+        return df
+
 def init():
+    """
+    初期化
+    """
     global fig, ax
     fig = plt.figure()
     #fig = plt.figure(figsize=(24,10), dpi=300, facecolor='w')
     ax = plt.subplot()
-init()
 
 def main():
-    print('hello')
+    """
+    main関数
+    """
     global fig, ax
     ohlc = np.vstack((range(len(df)), df.values.T)).T
     mpf.candlestick_ohlc(ax, ohlc, width=0.7, colorup='red', colordown='green')
@@ -58,14 +50,123 @@ def main():
     df['av_10'] = df['close'].rolling(window=term_10).mean()
     df['av_20'] = df['close'].rolling(window=term_20).mean()
     df['av_60'] = df['close'].rolling(window=term_60).mean()
-main()
 
+def pointCross(status = '5_20', str = '', current_flag = 0, previous_flag = 1):
+    """
+    ゴールデンクロス/デッドクロスしたタイミングの抽出
+    """
+    if (status == '5_20'):
+        ma = {'pointString':str+'_5_20', 1:'av_5', 2:'av_20'}
+    elif (status == '20_60'):
+        ma = {'pointString':str+'_20_60', 1:'av_20', 2:'av_60'}
+    else:
+        ma = {'pointString':str+'_5_20', 1:'av_5', 2:'av_20'}
+
+    for i, price in df.iterrows():
+        if (str == 'golden'):
+            current_flag = 1 if (price[ma[1]] > price[ma[2]]) else 0
+        else:
+            current_flag = 1 if (price[ma[1]] < price[ma[2]]) else 0
+
+        if (current_flag * (1 - previous_flag)):
+            df.loc[i, ma['pointString']] = price[ma[2]]
+        else:
+            df.loc[i, ma['pointString']] = None
+
+        previous_flag = current_flag
+
+def plotMA():
+    ax.plot(df.index, df['close'].rolling(5).mean(), color='r', label="MA(5)")
+    ax.plot(df.index, df['close'].rolling(7).mean(), color='black', label="MA(7)", linestyle=':')
+    ax.plot(df.index, df['close'].rolling(10).mean(), color='olive', label="MA(10)", linestyle=':')
+    ax.plot(df.index, df['close'].rolling(20).mean(), color='g', label="MA(20)")
+    ax.plot(df.index, df['close'].rolling(60).mean(), color='b', label="MA(60)")
+    ax.plot(df.index, df['close'].rolling(75).mean(), color='y', label="MA(75)")
+    ax.plot(df.index, df['close'].rolling(100).mean(), color='orange', label="MA(100)")
+    ax.plot(df.index, df['close'].rolling(200).mean(), color='gold', label="MA(200)")
+    ax.plot(df.index, df['close'].rolling(300).mean(), color='pink', label="MA(300)")
+    # ax.plot(df.index, pd.Series(df['close']).rolling(5).mean(), color='g', label="MA(5)")
+
+def scatterPoint():
+    #plt.scatter(50, 2500, s=100, marker="o",color='gold')
+    plt.scatter(x= df.index,y = df['golden_5_20'],marker='o',color='gold', s=200, label="GC_5_20")
+    plt.scatter(x= df.index,y = df['ded_5_20'],marker='o',color='black', s=200, label="DC_5_20")
+    plt.scatter(x= df.index,y = df['golden_20_60'],marker='o',color='orange', s=200, label="GC_20_60")
+    plt.scatter(x= df.index,y = df['ded_20_60'],marker='o',color='pink', s=200, label="DC_20_60")
+
+    plt.scatter(x= df.index,y = df['k_hanshin'],marker='^',color='dodgerblue', label="K_1")
+    plt.scatter(x= df.index,y = df['k_hanshin_2'],marker='^',color='cyan', label="K_2")
+    plt.scatter(x= df.index,y = df['k_hanshin_5'],marker='^',color='chartreuse', label="K_5")
+    plt.scatter(x= df.index,y = df['k_hanshin_6'],marker='^',color='darkviolet', label="K_6")
+    plt.scatter(x= df.index,y = df['gk_hanshin'],marker='v',color='dodgerblue', label="GK_1")
+    plt.scatter(x= df.index,y = df['gk_hanshin_2'],marker='v',color='cyan', label="GK_2")
+    plt.scatter(x= df.index,y = df['gk_hanshin_5'],marker='v',color='chartreuse', label="GK_5")
+    plt.scatter(x= df.index,y = df['gk_hanshin_6'],marker='v',color='darkviolet', label="GK_6")
+
+    plt.scatter(x= df.index,y = df['kka'],marker='4',color='olive')
+    plt.scatter(x= df.index,y = df['akk'],marker='4',color='olive')
+
+    cnt9 = df['cnt9'].values
+    print(cnt9)
+    for i, d in df.iterrows():
+        #print(d.cnt9)
+        marker = '$' + str(d.cnt9) + '$' if d.cnt9 is not np.nan else ""
+        plt.scatter(x= i,y = d.hight + 25,marker=marker,color='black')
+#        plt.scatter(x= df.index,y = df['hight'] + 75,marker='$' + str(9) + '$',color='black')
+
+def zoneColor(str = '', start_dt = '', end_dt = '', dt_20_60 = ''):
+    """
+    PPPゾーンの表示
+    """
+    color = 'red' if (str == 'golden') else 'blue'
+    if (str == 'golden'):
+        opposite = 'ded'
+    else:
+        opposite = 'golden'
+
+    for dt, price in df.iterrows():
+        start_flag = df.loc[dt][str+'_5_20']
+        end_flag = df.loc[dt][opposite+'_5_20']
+        status_20_60 = df.loc[dt][str+'_20_60']
+        if (np.isnan(start_flag) == False):
+            start_dt = dt
+            # pprint(dt)
+            #pprint(start_dt)
+
+        if (np.isnan(end_flag) == False):
+            end_dt = dt
+            # pprint(dt)
+            #pprint(end_dt)
+
+        if (np.isnan(status_20_60) == False):
+            dt_20_60 = dt
+            # pprint(dt)
+            #pprint(dt_20_60)
+
+        if (start_dt and end_dt):
+            ax.axvspan(start_dt, end_dt, facecolor=color, alpha=0.1)
+            if (dt_20_60 and end_dt):
+                ax.axvspan(dt_20_60, end_dt, facecolor=color, alpha=0.2)
+            strat_dt = ''
+            end_dt = ''
+            dt_20_60 = ''
+
+df = DataRead()
+df_ = df.copy()
+
+new = [dt.datetime.strptime(i, '%Y-%m-%d') for i in df_.index]
+df_.index = [mdates.date2num(i) for i in new]
+
+#df_.index = mdates.date2num(df_.index)
+data = df_.reset_index().values
+
+init()
+main()
 
 #cnt9 初期化
 cnt = 1
 for i in df.index:
     df.loc[i, 'cnt9'] = np.nan
-#print(df['cnt9'])
 
 for i in df.index:
     op = df['open']
@@ -134,30 +235,18 @@ for i in df.index:
     df.loc[i, 'kka'] = av5[i] * 0.97 if av5[i] > av7[i] > av10[i] else np.nan
     df.loc[i, 'akk'] = av5[i] * 1.03 if av5[i] < av7[i] < av10[i] else np.nan
 
-
+#シグナル点灯有無確認
+pprint('=====is np.nan?')
+for dt in df.index:
+    #if dt in ['2020-08-21', '2020-08-20', '2020-08-19', '2020-08-18', '2020-08-17', '2020-08-16']:
+    if dt in ['2020-08-21', '2020-08-20', '2020-08-19', '2020-08-18', '2020-08-17', '2020-08-16']:
+        now = df.index.get_loc(dt)  # 行番号取得
+        pprint(df.iloc[now]['k_hanshin_6'])
+        ret = dt if df.iloc[now]['k_hanshin_6'] > 0 else np.nan
+        break
+pprint(ret)
 #exit()
 
-# ゴールデンクロス/デッドクロスしたタイミングの抽出
-def pointCross(status = '5_20', str = '', current_flag = 0, previous_flag = 1):
-    if (status == '5_20'):
-        ma = {'pointString':str+'_5_20', 1:'av_5', 2:'av_20'}
-    elif (status == '20_60'):
-        ma = {'pointString':str+'_20_60', 1:'av_20', 2:'av_60'}
-    else:
-        ma = {'pointString':str+'_5_20', 1:'av_5', 2:'av_20'}
-
-    for i, price in df.iterrows():
-        if (str == 'golden'):
-            current_flag = 1 if (price[ma[1]] > price[ma[2]]) else 0
-        else:
-            current_flag = 1 if (price[ma[1]] < price[ma[2]]) else 0
-
-        if (current_flag * (1 - previous_flag)):
-            df.loc[i, ma['pointString']] = price[ma[2]]
-        else:
-            df.loc[i, ma['pointString']] = None
-
-        previous_flag = current_flag
 
 df['golden_5_20'] = 0
 df['golden_20_60'] = 0
@@ -168,132 +257,20 @@ pointCross('20_60', 'golden')
 pointCross('5_20', 'ded')
 pointCross('20_60', 'ded')
 
-def plotMA():
-    ax.plot(df.index, df['close'].rolling(5).mean(), color='r', label="MA(5)")
-    ax.plot(df.index, df['close'].rolling(7).mean(), color='black', label="MA(7)", linestyle=':')
-    ax.plot(df.index, df['close'].rolling(10).mean(), color='olive', label="MA(10)", linestyle=':')
-    ax.plot(df.index, df['close'].rolling(20).mean(), color='g', label="MA(20)")
-    ax.plot(df.index, df['close'].rolling(60).mean(), color='b', label="MA(60)")
-    ax.plot(df.index, df['close'].rolling(75).mean(), color='y', label="MA(75)")
-    ax.plot(df.index, df['close'].rolling(100).mean(), color='orange', label="MA(100)")
-    ax.plot(df.index, df['close'].rolling(200).mean(), color='gold', label="MA(200)")
-    ax.plot(df.index, df['close'].rolling(300).mean(), color='pink', label="MA(300)")
-    #ax.plot(df.index, pd.Series(df['close']).rolling(5).mean(), color='g', label="MA(5)")
+# 移動平均線表示
 plotMA()
 
-def scatterPoint():
-    #plt.scatter(50, 2500, s=100, marker="o",color='gold')
-    plt.scatter(x= df.index,y = df['golden_5_20'],marker='o',color='gold', s=200, label="GC_5_20")
-    plt.scatter(x= df.index,y = df['ded_5_20'],marker='o',color='black', s=200, label="DC_5_20")
-    plt.scatter(x= df.index,y = df['golden_20_60'],marker='o',color='orange', s=200, label="GC_20_60")
-    plt.scatter(x= df.index,y = df['ded_20_60'],marker='o',color='pink', s=200, label="DC_20_60")
-
-    plt.scatter(x= df.index,y = df['k_hanshin'],marker='^',color='dodgerblue', label="K_1")
-    plt.scatter(x= df.index,y = df['k_hanshin_2'],marker='^',color='cyan', label="K_2")
-    plt.scatter(x= df.index,y = df['k_hanshin_5'],marker='^',color='chartreuse', label="K_5")
-    plt.scatter(x= df.index,y = df['k_hanshin_6'],marker='^',color='darkviolet', label="K_6")
-    plt.scatter(x= df.index,y = df['gk_hanshin'],marker='v',color='dodgerblue', label="GK_1")
-    plt.scatter(x= df.index,y = df['gk_hanshin_2'],marker='v',color='cyan', label="GK_2")
-    plt.scatter(x= df.index,y = df['gk_hanshin_5'],marker='v',color='chartreuse', label="GK_5")
-    plt.scatter(x= df.index,y = df['gk_hanshin_6'],marker='v',color='darkviolet', label="GK_6")
-
-    plt.scatter(x= df.index,y = df['kka'],marker='4',color='olive')
-    plt.scatter(x= df.index,y = df['akk'],marker='4',color='olive')
-
-    cnt9 = df['cnt9'].values
-    print(cnt9)
-    for i, d in df.iterrows():
-        #print(d.cnt9)
-        marker = '$' + str(d.cnt9) + '$' if d.cnt9 is not np.nan else ""
-        plt.scatter(x= i,y = d.hight + 25,marker=marker,color='black')
-#        plt.scatter(x= df.index,y = df['hight'] + 75,marker='$' + str(9) + '$',color='black')
+#シグナル表示
 scatterPoint()
 
 ax.grid()
 ax.set_xlim(-1, len(df))
 fig.autofmt_xdate()
 
-#axvspan
-#PPPゾーンの表示
-def zoneColor(str = '', start_dt = '', end_dt = '', dt_20_60 = ''):
-    color = 'red' if (str == 'golden') else 'blue'
-    if (str == 'golden'):
-        opposite = 'ded'
-    else:
-        opposite = 'golden'
-
-    for dt, price in df.iterrows():
-        start_flag = df.loc[dt][str+'_5_20']
-        end_flag = df.loc[dt][opposite+'_5_20']
-        status_20_60 = df.loc[dt][str+'_20_60']
-        if (np.isnan(start_flag) == False):
-            start_dt = dt
-            # pprint(dt)
-            #pprint(start_dt)
-
-        if (np.isnan(end_flag) == False):
-            end_dt = dt
-            # pprint(dt)
-            #pprint(end_dt)
-
-        if (np.isnan(status_20_60) == False):
-            dt_20_60 = dt
-            # pprint(dt)
-            #pprint(dt_20_60)
-
-        if (start_dt and end_dt):
-            ax.axvspan(start_dt, end_dt, facecolor=color, alpha=0.1)
-            if (dt_20_60 and end_dt):
-                ax.axvspan(dt_20_60, end_dt, facecolor=color, alpha=0.2)
-            strat_dt = ''
-            end_dt = ''
-            dt_20_60 = ''
-
-
+# PPPゾーンの表示
 ppp = np.array( [] )
 zoneColor('golden')
 zoneColor('ded')
-
-#ax.axvspan(start_dt, end_dt, facecolor = "red", alpha=0.2)
-
-x = 2
-y = 2000
-plt.plot(x, y, marker='.', color='b')
-#plt.axvline(x=1, color='b')
-
-def pressKey(event):
-    global x, y
-    if (event.key == 'right'):
-        x += 1
-    if (event.key == 'left'):
-        x -= 1
-    if (event.key == 'up'):
-        y += 20
-    if (event.key == 'down'):
-        y -= 20
-
-    #plt.plot(x, y, marker='.', color='r')
-    plt.axvline(x=x, color='black')
-    plt.pause(0.1)
-    main()
-    pprint(event.key)
-
-cid = fig.canvas.mpl_connect('key_press_event', pressKey)
-
-def motion(event):
-    x = event.xdata
-    y = event.ydata
-    ln_v.set_xdata(x)
-    ln_h.set_ydata(y)
-    plt.draw()
-
-#ln_v = plt.axvline(0)
-#ln_h = plt.axhline(0)
-#fig.canvas.mpl_connect('motion_notify_event', motion)
-
-#Show full screen
-#mng = plt.get_current_fig_manager()
-#mng.full_screen_toggle()
 
 # base
 plt.legend()
